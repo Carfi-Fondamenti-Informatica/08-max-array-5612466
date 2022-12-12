0a7efb72251b8f225c53a261dc4433b9de61fc48
5612466 <115354030+5612466@users.noreply.github.com>
2022-12-12 15:52:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include "lib.h"
+
+float max(float numero[], int a) {
+
+    float cont = numero[0];
+    for (int i = 0; i < n; i++) {
+        if (numero[i] > cont) {
+            cont = numero[i];
+        }
+    }
+    return cont;
+}
